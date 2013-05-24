8472a26444d4f8733ff32efe2d47bdabdbf7fbdd
Jeff Layton <jlayton@redhat.com>
2013-05-24 07:40:59 -0400

    cifs: add a "nosharesock" mount option to force new sockets to server to be created
    Some servers set max_vcs to 1 and actually do enforce that limit. Add a
    new mount option to work around this behavior that forces a mount
    request to open a new socket to the server instead of reusing an
    existing one.
    
    I'd prefer to come up with a solution that doesn't require this, so
    consider this a debug patch that you can use to determine whether this
    is the real problem.
    
    Cc: Jim McDonough <jmcd@samba.org>
    Cc: Steve French <smfrench@gmail.com>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -449,6 +449,7 @@ struct smb_vol {
 	bool mfsymlinks:1; /* use Minshall+French Symlinks */
 	bool multiuser:1;
 	bool rwpidforward:1; /* pid forward for read/write operations */
+	bool nosharesock;
 	unsigned int rsize;
 	unsigned int wsize;
 	bool sockopt_tcp_nodelay:1;
