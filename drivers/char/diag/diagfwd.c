c2189ed439be7cc2ecfd8ebbde558ad74eeaafae
Jeff Hugo <jhugo@codeaurora.org>
2014-01-09 13:13:17 -0700

    msm: smd: Support multiplatform
    Upstream prefers existing drivers be converted to support multiplatform
    kernels.  This requires drivers to be located in generic functionality
    directories instead of specific mach directories.
    
    Move the smd and smsm drivers to the drivers/soc/qcom location to support
    multiplatform.
    
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -27,7 +27,7 @@
 #ifdef CONFIG_DIAG_OVER_USB
 #include <mach/usbdiag.h>
 #endif
-#include <mach/msm_smd.h>
+#include <soc/qcom/smd.h>
 #include <soc/qcom/socinfo.h>
 #include <mach/restart.h>
 #include "diagmem.h"
