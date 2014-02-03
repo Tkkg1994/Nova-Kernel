f1300f3437522afd7d3dc30e2b50517e320a352e
Rusty Russell <rusty@rustcorp.com.au>
2014-02-03 11:15:13 +1030

    module: remove MODULE_GENERIC_TABLE
    MODULE_DEVICE_TABLE() calles MODULE_GENERIC_TABLE(); make it do the
    work directly.  This also removes a wart introduced in the last patch,
    where the alias is defined to be an unknown struct type "struct
    type##__##name##_device_id" instead of "struct type##_device_id" (it's
    an extern so GCC doesn't care, but it's wrong).
    
    The other user of MODULE_GENERIC_TABLE (ISAPNP_CARD_TABLE) is unused,
    so delete it.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -82,15 +82,6 @@ void sort_extable(struct exception_table_entry *start,
 void sort_main_extable(void);
 void trim_init_extable(struct module *m);
 
-#ifdef MODULE
-#define MODULE_GENERIC_TABLE(gtype, name)			\
-extern const struct gtype##_id __mod_##gtype##_table		\
-  __attribute__ ((unused, alias(__stringify(name))))
-
-#else  /* !MODULE */
-#define MODULE_GENERIC_TABLE(gtype, name)
-#endif
-
 /* Generic info of form tag = "info" */
 #define MODULE_INFO(tag, info) __MODULE_INFO(tag, tag, info)
 
