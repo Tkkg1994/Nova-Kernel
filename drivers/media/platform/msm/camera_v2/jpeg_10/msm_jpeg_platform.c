a0a77876a5b5c0771dc7d395d8cb5a5097f77751
Larry Bassel <lbassel@codeaurora.org>
2014-02-10 14:17:29 -0800

    iommu: move iommu header files to new locations
    Header files are no longer allowed in the directory
    arch/arm/mach-msm/include/mach/ .
    
    Move the iommu related header files to a more suitable place.
    
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_platform.c
+++ drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_platform.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -22,6 +22,7 @@
 #include <linux/msm_iommu_domains.h>
 #include <linux/msm-bus.h>
 #include <linux/msm-bus-board.h>
+#include <linux/iommu.h>
 
 #include "msm_jpeg_platform.h"
 #include "msm_jpeg_sync.h"
