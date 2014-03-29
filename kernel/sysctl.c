2fc1c1e9603fc552f18af94575a9a51630a4da01
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 16:56:45 -0700

    sched: Introduce CONFIG_SCHED_FREQ_INPUT
    Introduce a compile time flag to enable scheduler guidance of
    frequency selection. This flag is also used to turn on or off
    window-based load stats feature.
    
    Having a compile time flag will let some platforms avoid any
    overhead that may be present with this scheduler feature.
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -299,6 +299,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#ifdef CONFIG_SCHED_FREQ_INPUT
 	{
 		.procname       = "sched_window_stats_policy",
 		.data           = &sysctl_sched_window_stats_policy,
@@ -313,6 +314,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
