98c675019fd6e0071a3afd38ebfca5357ee9d7f4
Srinivas Rao L <lsrao@codeaurora.org>
2014-12-18 17:38:16 +0530

    cpuidle: lpm-workarounds: Add module lpm-workarounds
    Add new module lpm-workarounds to support workarounds for
    low power modes driver. Add workaround support for unvote
    CX turbo vote during exit of rpm assisted power collapse.
    On some targets rpm bumps up the CX to turbo while
    coming out of power collapse, to reduce the power impact
    of this the workaround is necessary.
    
    Signed-off-by: Srinivas Rao L <lsrao@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -45,6 +45,7 @@
 #include <asm/arch_timer.h>
 #include <asm/cacheflush.h>
 #include "lpm-levels.h"
+#include "lpm-workarounds.h"
 #include <trace/events/power.h>
 
 #ifdef CONFIG_SEC_GPIO_DVS
@@ -604,6 +605,13 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	level = &cluster->levels[cluster->last_level];
 	if (level->notify_rpm) {
 		msm_rpm_exit_sleep();
+
+		/* If RPM bumps up CX to turbo, unvote CX turbo vote
+		 * during exit of rpm assisted power collapse to
+		 * reduce the power impact
+		 */
+
+		lpm_wa_cx_unvote_send();
 		msm_mpm_exit_sleep(from_idle);
 	}
 
