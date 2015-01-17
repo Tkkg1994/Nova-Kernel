e74e4daa36b8762e06e1f081de3b8df871667a16
placiano <nextblueteam@outlook.es>
2015-01-18 00:29:01 +0100

    Voltage Control: generic voltage control for DTS based kernels
    Thanks to faux123
    
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -32,6 +32,10 @@
 #include <trace/events/power.h>
 #include <linux/msm-bus.h>
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+static struct cpufreq_frequency_table *dts_freq_table;
+#endif
+
 static DEFINE_MUTEX(l2bw_lock);
 
 static struct clk *cpu_clk[NR_CPUS];
@@ -545,6 +549,20 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 	ftbl[i].driver_data = i;
 	ftbl[i].frequency = CPUFREQ_TABLE_END;
 
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+    dts_freq_table =
+    devm_kzalloc(dev, (nf + 1) *
+                 sizeof(struct cpufreq_frequency_table),
+                 GFP_KERNEL);
+    
+    if (!dts_freq_table)
+        return -ENOMEM;
+    
+    for (i = 0, j = 0; i < nf; i++, j += 3)
+        dts_freq_table[i].frequency = data[j];
+    dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
+#endif
+
 	if (ports)
 		devm_kfree(dev, ports);
 
@@ -552,6 +570,22 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 
 	return ftbl;
 }
+
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+bool is_used_by_scaling(unsigned int freq)
+{
+    unsigned int i, cpu_freq;
+    
+    for (i = 0; dts_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
+        cpu_freq = dts_freq_table[i].frequency;
+        if (cpu_freq == CPUFREQ_ENTRY_INVALID)
+            continue;
+        if (freq == cpu_freq)
+            return true;
+    }
+    return -EINVAL;
+}
+#endif
 
 static int __init msm_cpufreq_probe(struct platform_device *pdev)
 {
