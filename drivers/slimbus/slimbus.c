16bf4644d8c7ba340cf70559ed12af2331f7e7de
Dilip Kota <c_dkota@codeaurora.org>
2015-02-03 19:21:31 +0530

    slim-msm: Configure push pull protocol on MSM Controller
    Configures push pull protocol parameters on
    MSM controller. It makes sure to program the
    push pull protocol per channel in the framework.
    Irresepective of client request, configure to
    ISO protocol if channel frequency is multiple
    of super frame frequency.
    
    Signed-off-by: Dilip Kota <dkota@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/slimbus/slimbus.c
+++ drivers/slimbus/slimbus.c
@@ -1324,6 +1324,7 @@ static int disconnect_port_ch(struct slim_controller *ctrl, u32 ph)
 		ctrl->ports[pn].state = SLIM_P_UNCFG;
 		ctrl->ports[pn].cfg.watermark = 0;
 		ctrl->ports[pn].cfg.port_opts = 0;
+		ctrl->ports[pn].ch = NULL;
 	}
 	return 0;
 }
@@ -1348,6 +1349,7 @@ int slim_connect_src(struct slim_device *sb, u32 srch, u16 chanh)
 	struct slim_ich *slc = &ctrl->chans[chan];
 	enum slim_port_flow flow = SLIM_HDL_TO_FLOW(srch);
 	u8 la = SLIM_HDL_TO_LA(srch);
+	u8 pn = SLIM_HDL_TO_PORT(srch);
 
 	/* manager ports don't have direction when they are allocated */
 	if (la != SLIM_LA_MANAGER && flow != SLIM_SRC)
@@ -1356,7 +1358,6 @@ int slim_connect_src(struct slim_device *sb, u32 srch, u16 chanh)
 	mutex_lock(&ctrl->sched.m_reconf);
 
 	if (la == SLIM_LA_MANAGER) {
-		u8 pn = SLIM_HDL_TO_PORT(srch);
 		if (pn >= ctrl->nports ||
 			ctrl->ports[pn].state != SLIM_P_UNCFG) {
 			ret = -EINVAL;
@@ -1377,7 +1378,7 @@ int slim_connect_src(struct slim_device *sb, u32 srch, u16 chanh)
 		ret = -EALREADY;
 		goto connect_src_err;
 	}
-
+	ctrl->ports[pn].ch = &slc->prop;
 	ret = connect_port_ch(ctrl, chan, srch, SLIM_SRC);
 
 	if (!ret)
@@ -1436,8 +1437,10 @@ int slim_connect_sink(struct slim_device *sb, u32 *sinkh, int nsink, u16 chanh)
 				(pn >= ctrl->nports ||
 				ctrl->ports[pn].state != SLIM_P_UNCFG))
 				ret = -EINVAL;
-		else
+		else {
+			ctrl->ports[pn].ch = &slc->prop;
 			ret = connect_port_ch(ctrl, chan, sinkh[j], SLIM_SINK);
+		}
 		if (ret) {
 			for (j = j - 1; j >= 0; j--)
 				disconnect_port_ch(ctrl, sinkh[j]);
@@ -1630,8 +1633,10 @@ static int slim_remove_ch(struct slim_controller *ctrl, struct slim_ich *slc)
 		 * disconnect. It is client's responsibility to call disconnect
 		 * on ports owned by the slave device
 		 */
-		if (la == SLIM_LA_MANAGER)
+		if (la == SLIM_LA_MANAGER) {
 			ctrl->ports[SLIM_HDL_TO_PORT(ph)].state = SLIM_P_UNCFG;
+			ctrl->ports[SLIM_HDL_TO_PORT(ph)].ch = NULL;
+		}
 	}
 
 	ph = slc->srch;
