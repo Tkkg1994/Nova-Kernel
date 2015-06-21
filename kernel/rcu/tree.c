89fdde87f0804404910b1422f277aec441414190
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-06-21 23:51:21 +0530

    rcu: Avoid redundant grace-period kthread wakeups
    When setting up an in-the-future "advanced" grace period, the code needs
    to wake up the relevant grace-period kthread, which it currently does
    unconditionally.  However, this results in needless wakeups in the case
    where the advanced grace period is being set up by the grace-period
    kthread itself, which is a non-uncommon situation.  This commit therefore
    checks to see if the running thread is the grace-period kthread, and
    avoids doing the irq_work_queue()-mediated wakeup in that case.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: 1eafd31c640d6799c63136246a59d608bed93c74
    [kishank@codeaurora.org resolve trivial conflicts]
    Signed-off-by: Kishan Kumar <kishank@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -1629,8 +1629,9 @@ rcu_start_gp_advanced(struct rcu_state *rsp, struct rcu_node *rnp,
 
 	/*
 	 * We can't do wakeups while holding the rnp->lock, as that
-	 * could cause possible deadlocks with the rq->lock. Deter
-	 * the wakeup to interrupt context.
+	 * could cause possible deadlocks with the rq->lock. Defer
+	 * the wakeup to interrupt context.  And don't bother waking
+	 * up the running kthread.
 	 */
 	return true;
 }
