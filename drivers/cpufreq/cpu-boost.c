98ec363b46d29d38166746e2637f2d2ef598544b
Pranav Vashi <neobuddy89@gmail.com>
2015-08-24 12:30:34 +0530

    msm: Fix some trivial warnings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -57,7 +57,9 @@ static struct workqueue_struct *cpu_boost_wq;
 
 static struct work_struct input_boost_work;
 
+#ifdef CONFIG_STATE_NOTIFIER
 static struct notifier_block notif;
+#endif
 
 static unsigned int boost_ms;
 module_param(boost_ms, uint, 0644);
@@ -79,8 +81,10 @@ module_param(load_based_syncs, bool, 0644);
 static bool hotplug_boost = 1;
 module_param(hotplug_boost, bool, 0644);
 
+#ifdef CONFIG_STATE_NOTIFIER
 bool wakeup_boost;
 module_param(wakeup_boost, bool, 0644);
+#endif
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
@@ -518,7 +522,7 @@ static int cpuboost_cpu_callback(struct notifier_block *cpu_nb,
 		if (!hotplug_boost || !input_boost_enabled ||
 		     work_pending(&input_boost_work))
 			break;
-		pr_debug("Hotplug boost for CPU%d\n", (int)hcpu);
+		pr_debug("Hotplug boost for CPU%lu\n", (long)hcpu);
 		queue_work(cpu_boost_wq, &input_boost_work);
 		last_input_time = ktime_to_us(ktime_get());
 		break;
@@ -532,6 +536,7 @@ static struct notifier_block __refdata cpu_nblk = {
         .notifier_call = cpuboost_cpu_callback,
 };
 
+#ifdef CONFIG_STATE_NOTIFIER
 static void __wakeup_boost(void)
 {
 	if (!wakeup_boost || !input_boost_enabled ||
@@ -542,7 +547,6 @@ static void __wakeup_boost(void)
 	last_input_time = ktime_to_us(ktime_get());
 }
 
-#ifdef CONFIG_STATE_NOTIFIER
 static int state_notifier_callback(struct notifier_block *this,
 				unsigned long event, void *data)
 {
