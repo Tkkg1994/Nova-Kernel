f846b88c8d3e2a1c7d7dee695839d97d73fc4fdc
placiano <placiano80@gmail.com>
2015-09-06 20:29:55 +0200

    sound/fauxsound: Updates
    1.- Ignore checksums
    	so you can easily adjust the gain controls in /sys/kernel/sound_control_3
    2.- Add sanity checks, including stripping negative values
    3.- Only worry about negative values
    
    Thanks to ffolkes
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -186,6 +186,8 @@ static bool calc_checksum(unsigned int a, unsigned int b, unsigned int c)
 	unsigned char chksum = 0;
 
 	chksum = ~((a & 0xff) + (b & 0xff));
+	
+	return true;
 
 	if (chksum == (c & 0xff)) {
 		return true;
@@ -254,9 +256,15 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 static ssize_t speaker_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	unsigned int lval, rval, chksum;
+	int lval, rval, chksum;
 
-	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
+	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
+	
+	if (lval < 0)
+		lval = 0;
+	
+	if (rval < 0)
+		rval = 0;
 
 	if (calc_checksum(lval, rval, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
@@ -280,9 +288,15 @@ static ssize_t headphone_gain_show(struct kobject *kobj,
 static ssize_t headphone_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	unsigned int lval, rval, chksum;
+	int lval, rval, chksum;
 
-	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
+	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
+	
+	if (lval < 0)
+		lval = 0;
+	
+	if (rval < 0)
+		rval = 0;
 
 	if (calc_checksum(lval, rval, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
@@ -304,11 +318,17 @@ static ssize_t headphone_pa_gain_show(struct kobject *kobj,
 static ssize_t headphone_pa_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	unsigned int lval, rval, chksum;
+	int lval, rval, chksum;
 	unsigned int gain, status;
 	unsigned int out;
 
-	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
+	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
+	
+	if (lval < 0)
+		lval = 0;
+	
+	if (rval < 0)
+		rval = 0;
 
 	if (calc_checksum(lval, rval, chksum)) {
 	gain = taiko_read(fauxsound_codec_ptr, TAIKO_A_RX_HPH_L_GAIN);
