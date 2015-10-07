71510655841020f61050a02a61f2971a90787bf3
Tkkg1994 <luca.grifo@outlook.com>
2015-10-08 01:11:02 +0200

    Revert "ARM: constify machine_desc structure uses"
    This reverts commit 50146b5d0ad599f1e173ec048969e349915d420a.

--- arch/arm/include/asm/mach/arch.h
+++ arch/arm/include/asm/mach/arch.h
@@ -59,12 +59,12 @@ struct machine_desc {
 /*
  * Current machine - only accessible during boot.
  */
-extern const struct machine_desc *machine_desc;
+extern struct machine_desc *machine_desc;
 
 /*
  * Machine type table - also only accessible during boot
  */
-extern const struct machine_desc __arch_info_begin[], __arch_info_end[];
+extern struct machine_desc __arch_info_begin[], __arch_info_end[];
 #define for_each_machine_desc(p)			\
 	for (p = __arch_info_begin; p < __arch_info_end; p++)
 
