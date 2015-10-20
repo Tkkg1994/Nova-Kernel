4da124a97fe5b8685d9e85f4f9f9269cd685ec2e
Pranav Vashi <neobuddy89@gmail.com>
2015-10-20 21:02:02 +0530

    selinux: Prevent init process enforcing selinux
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -166,6 +166,10 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	length = -EINVAL;
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
+
+	if (strcmp(current->comm, "init") == 0)
+		goto out;
+
 #ifdef CONFIG_ALWAYS_ENFORCE
 	// If build is user build and enforce option is set, selinux is always enforcing
 	new_value = 1;
@@ -182,6 +186,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 #else
 	// If build is user build and nothing special is set, selinux will always be permissive
 	new_value = 0;
+
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
