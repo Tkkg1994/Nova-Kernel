63936ab4644b2ba54a132d95b73c67391820c3e3
Tkkg1994 <luca.grifo@outlook.com>
2015-12-25 00:50:05 +0100

    cpufreq: add blueactive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -541,6 +541,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RACCOON_CITY)
 extern struct cpufreq_governor cpufreq_gov_raccoon_city;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_raccoon_city)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
+extern struct cpufreq_governor cpufreq_gov_bluactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bluactive)
 #endif
 
 /*********************************************************************
