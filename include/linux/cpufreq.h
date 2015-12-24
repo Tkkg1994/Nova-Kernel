2c896739391f42a7973636f910292da0805b588a
Tkkg1994 <luca.grifo@outlook.com>
2015-12-25 00:37:46 +0100

    cpufreq: add umbrella_core cpufreq driver

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -532,6 +532,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive_pro;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UMBRELLA_CORE)
+extern struct cpufreq_governor cpufreq_gov_umbrella_core;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_umbrella_core)
 #endif
 
 /*********************************************************************
