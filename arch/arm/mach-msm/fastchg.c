ecb5175fa243dcbee05ee5cfb9d4df61eab41826
Tkkg1994 <luca.grifo@outlook.com>
2015-12-25 18:41:45 +0100

    fastchg: change some values to enable fastcharging by default

--- arch/arm/mach-msm/fastchg.c
+++ arch/arm/mach-msm/fastchg.c
@@ -480,13 +480,13 @@ int force_fast_charge_init(void)
 {
 	int force_fast_charge_retval;
 
-	force_fast_charge		= FAST_CHARGE_DISABLED; /* Forced fast charge disabled by default */
-	use_mtp_during_fast_charge	= USE_MTP_DURING_FAST_CHARGE_ENABLED; /* Use MTP during fast charge, enabled by default */
-	screen_on_current_limit		= SCREEN_ON_CURRENT_LIMIT_ENABLED; /* Use Samsung Screen ON current limit while charging, enabled by default */
-	ac_charge_level			= AC_CHARGE_1800;	      /* Default AC charge level to 1800mA/h    */
-	usb_charge_level		= USB_CHARGE_460;	      /* Default USB charge level to 460mA/h    */
-	wireless_charge_level		= WIRELESS_CHARGE_650;  /* Default USB charge level to 650mA/h    */
-	failsafe			= FAIL_SAFE_ENABLED;    /* Allow only values in list by default   */
+	force_fast_charge		= FAST_CHARGE_FORCE_CUSTOM_MA;		/* Forced fast charge enabled by default */
+	use_mtp_during_fast_charge	= USE_MTP_DURING_FAST_CHARGE_ENABLED;	/* Use MTP during fast charge, enabled by default */
+	screen_on_current_limit		= SCREEN_ON_CURRENT_LIMIT_DISABLED;	/* Use Samsung Screen ON current limit while charging, disabled by default */
+	ac_charge_level			= AC_CHARGE_2000;			/* Default AC charge level to 2000mA/h    */
+	usb_charge_level		= USB_CHARGE_800;			/* Default USB charge level to 800mA/h    */
+	wireless_charge_level		= WIRELESS_CHARGE_650;			/* Default USB charge level to 650mA/h    */
+	failsafe			= FAIL_SAFE_ENABLED;			/* Allow only values in list by default   */
 
         force_fast_charge_kobj =
 		kobject_create_and_add("fast_charge", kernel_kobj);
