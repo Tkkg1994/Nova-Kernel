neobuddy89/shamu

msm: mdss: hdmi: do not phy reset if splash screen enabled
When splash screen is enabled (which is enabled for hdmi primary),
LK will do the phy reset. Do not reset phy in kernel in this case.

Change-Id: I186e97b30bfd907b47f6d6dd7f478adc2d5d6f6b
Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -1352,7 +1352,8 @@ static void hdmi_tx_hpd_int_work(struct work_struct *work)
 		return;
 	}
 	io = &hdmi_ctrl->pdata.io[HDMI_TX_CORE_IO];
-	DEV_DBG("%s: Got HPD interrupt\n", __func__);
+	DEV_DBG("%s: Got HPD %s interrupt\n", __func__,
+		hdmi_ctrl->hpd_state ? "connect" : "disconnect");
 
 	if (hdmi_ctrl->hpd_state) {
 		/*
@@ -2848,6 +2849,8 @@ static void hdmi_tx_hpd_off(struct hdmi_tx_ctrl *hdmi_ctrl)
 	spin_unlock_irqrestore(&hdmi_ctrl->hpd_state_lock, flags);
 
 	hdmi_ctrl->hpd_initialized = false;
+
+	DEV_DBG("%s\n", __func__);
 } /* hdmi_tx_hpd_off */
 
 static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
@@ -2879,7 +2882,7 @@ static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
 
 		dss_reg_dump(io->base, io->len, "HDMI-INIT: ", REG_DUMP);
 
-		if (!hdmi_ctrl->panel_data.panel_info.cont_splash_enabled) {
+		if (!hdmi_ctrl->pdata.primary) {
 			hdmi_tx_set_mode(hdmi_ctrl, false);
 			hdmi_tx_phy_reset(hdmi_ctrl);
 			hdmi_tx_set_mode(hdmi_ctrl, true);
@@ -2903,6 +2906,8 @@ static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
 
 		hdmi_tx_hpd_polarity_setup(hdmi_ctrl, HPD_CONNECT_POLARITY);
 	}
+
+	DEV_DBG("%s\n", __func__);
 
 	return rc;
 } /* hdmi_tx_hpd_on */

Tkkg1994/Nova-Kernel

Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -1397,7 +1397,8 @@ static void hdmi_tx_hpd_int_work(struct work_struct *work)
 		return;
 	}
 	io = &hdmi_ctrl->pdata.io[HDMI_TX_CORE_IO];
-	DEV_INFO("%s: Got HPD interrupt\n", __func__);
+	DEV_INFO("%s: Got HPD %s interrupt\n", __func__,
+		hdmi_ctrl->hpd_state ? "connect" : "disconnect");
 
 #if defined(CONFIG_SEC_MHL_SUPPORT)
 	current_jiffies = get_jiffies_64();
@@ -2929,6 +2930,8 @@ static void hdmi_tx_hpd_off(struct hdmi_tx_ctrl *hdmi_ctrl)
 	spin_unlock_irqrestore(&hdmi_ctrl->hpd_state_lock, flags);
 
 	hdmi_ctrl->hpd_initialized = false;
+
+	DEV_DBG("%s\n", __func__);
 } /* hdmi_tx_hpd_off */
 
 static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
@@ -2960,7 +2963,7 @@ static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
 
 		dss_reg_dump(io->base, io->len, "HDMI-INIT: ", REG_DUMP);
 
-		if (!hdmi_ctrl->panel_data.panel_info.cont_splash_enabled) {
+		if (!hdmi_ctrl->pdata.primary) {
 			hdmi_tx_set_mode(hdmi_ctrl, false);
 			hdmi_tx_phy_reset(hdmi_ctrl);
 			hdmi_tx_set_mode(hdmi_ctrl, true);
@@ -2984,6 +2987,8 @@ static int hdmi_tx_hpd_on(struct hdmi_tx_ctrl *hdmi_ctrl)
 
 		hdmi_tx_hpd_polarity_setup(hdmi_ctrl, HPD_CONNECT_POLARITY);
 	}
+
+	DEV_DBG("%s\n", __func__);
 
 	return rc;
 } /* hdmi_tx_hpd_on */