c305b1a268799d20a0f852ac6c65553781482e3f
Tkkg1994 <luca.grifo@outlook.com>
2016-01-12 17:28:59 +0100

    base: update wakeup sources thanks to @Neobuddy89
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -18,18 +18,18 @@
 #include <trace/events/power.h>
 #include <linux/moduleparam.h>
 
-static bool enable_si_ws = false;
+static bool enable_si_ws = true;
 module_param(enable_si_ws, bool, 0644);
-static bool enable_msm_hsic_ws = false;
+static bool enable_msm_hsic_ws = true;
 module_param(enable_msm_hsic_ws, bool, 0644);
-static bool enable_wlan_rx_wake_ws = false;
+static bool enable_wlan_rx_wake_ws = true;
 module_param(enable_wlan_rx_wake_ws, bool, 0644);
-static bool enable_wlan_ctrl_wake_ws = false;
+static bool enable_wlan_ctrl_wake_ws = true;
 module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
-static bool enable_wlan_wake_ws = false;
+static bool enable_wlan_wake_ws = true;
 module_param(enable_wlan_wake_ws, bool, 0644);
-static bool enable_bluedroid_timer_ws = false;
-module_param(enable_bluedroid_timer_ws, bool, 0644);
+static bool enable_bluesleep_ws = true;
+module_param(enable_bluesleep_ws, bool, 0644);
 
 #include "power.h"
 
@@ -481,38 +481,40 @@ static bool wakeup_source_not_registered(struct wakeup_source *ws)
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
-	
-	if (!enable_si_ws && !strcmp(ws->name, "sensor_ind"))
-		return;
-
-	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host"))
-                return;
-
-	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
-                return;
-
-	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
-                return;
-
-	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
-                return;
-
-	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
-		return;
-
-	if (!enable_si_ws && !strcmp(ws->name, "sensor_ind")) {
-		pr_info("wakeup source sensor_ind activate skipped\n");
-		return;
-	}
-
-	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) {
-                pr_info("wakeup source msm_hsic_host activate skipped\n");
-                return;
-        }
 
 	if (WARN(wakeup_source_not_registered(ws),
 			"unregistered wakeup source\n"))
 		return;
+
+	if (!enable_si_ws && !strcmp(ws->name, "sensor_ind")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+		return;
+	}
+
+	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+                return;
+        }
+
+	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+                return;
+	}
+
+	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+                return;
+	}
+
+	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+                return;
+	}
+
+	if (!enable_bluesleep_ws && !strcmp(ws->name, "bluesleep")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+		return;
+	}
 
 	/*
 	 * active wakeup source should bring the system
