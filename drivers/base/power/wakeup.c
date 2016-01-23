36bca4514dff7b7d82d11999a1021128e81f31c8
Jesse Chan <cjx123@outlook.com>
2016-01-23 19:24:01 +0800

    wakeup: add ssp_ws

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -32,6 +32,8 @@ static bool enable_bluesleep_ws = true;
 module_param(enable_bluesleep_ws, bool, 0644);
 static bool enable_ssp_sensorhub_ws = true;
 module_param(enable_ssp_sensorhub_ws, bool, 0644);
+static bool enable_ssp_ws = true;
+module_param(enable_ssp_ws, bool, 0644);
 
 #include "power.h"
 
@@ -518,7 +520,12 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		return;
 	}
 
-	if (!enable_ssp_sensorhub_ws && !strcmp(ws->name, "ssp_wake_lock")) {
+	if (!enable_ssp_sensorhub_ws && !strcmp(ws->name, "ssp_sensorhub_wake_lock")) {
+		pr_info("wakeup source %s activate skipped\n",ws->name);
+		return;
+	}
+
+	if (!enable_ssp_ws && !strcmp(ws->name, "ssp_wake_lock")) {
 		pr_info("wakeup source %s activate skipped\n",ws->name);
 		return;
 	}
