673c2279025ad34d9466ebb351666c739893e15f
Tkkg1994 <luca.grifo@outlook.com>
2016-02-07 19:06:44 +0100

    Revert "cfg80211: add missing break in cfg80211_get_chan_state()"
    This reverts commit f365425db33c701dc6235b8b1dace6a53d5859ac.

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -532,7 +532,6 @@ cfg80211_get_chan_state(struct wireless_dev *wdev,
 				  : CHAN_MODE_EXCLUSIVE;
 			return;
 		}
-		break;
 	case NL80211_IFTYPE_STATION:
 	case NL80211_IFTYPE_P2P_CLIENT:
 		if (wdev->current_bss) {
