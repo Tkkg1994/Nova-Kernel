5a018e430758023549a2739fbb9e413f47cd8da9
Tkkg1994 <luca.grifo@outlook.com>
2016-02-27 17:49:44 +0100

    add autosmp and update blu_plug https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/e95159e687028dbb56ee51ceff7a6c885b934007

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -29,15 +29,15 @@
 #define INIT_DELAY		(60 * HZ) /* Initial delay to 60 sec, 4 cores while boot */
 #define DELAY			(HZ / 2)
 #define UP_THRESHOLD		(80)
-#define MIN_ONLINE		(2)
+#define MIN_ONLINE		(1)
 #define MAX_ONLINE		(4)
 #define DEF_DOWN_TIMER_CNT	(6)	/* 3 secs */
 #define DEF_UP_TIMER_CNT	(2)	/* 1 sec */
-#define MAX_CORES_SCREENOFF (2)
-#define MAX_FREQ_SCREENOFF (1190400)
-#define MAX_FREQ_PLUG (2649600)
-#define DEF_PLUG_THRESHOLD 0
-#define BLU_PLUG_ENABLED	0
+#define MAX_CORES_SCREENOFF     (1)
+#define MAX_FREQ_SCREENOFF      (0)
+#define MAX_FREQ_PLUG           (2649600)
+#define DEF_PLUG_THRESHOLD      (70)
+#define BLU_PLUG_ENABLED	(0)
 
 static unsigned int blu_plug_enabled = BLU_PLUG_ENABLED;
 
@@ -502,6 +502,9 @@ static int set_enabled(const char *val, const struct kernel_param *kp)
 		return -EINVAL;
 	if (i < 0 || i > 1)
 		return 0;
+		
+	if (i == blu_plug_enabled)
+		return i;
 
 	ret = param_set_uint(val, kp);
 	blu_plug_enabled = i;
